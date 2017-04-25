efaa7a1f48801f0fb44ba4090def7d7adc831132
Michal Ormos <mi.ormos@gmail.com>
2017-04-25 11:06:38 +0200

    Adding missing radar_equation.m

--- radar_equation.m
+++ radar_equation.m
@@ -4,7 +4,7 @@
 %
 % power_recei     % Pr % Received power [W]
 % power_trans     % Pt % Transmitter peak power [W]
-% trans_gain      % Ft % Transmitter gain [Hz]
+% f_trans      % Ft % Transmitter gain [Hz]
 % recei_gain      % Fr % Receiver gain [Hz]
 % RCS             % RCS % Radar Cross Section - object reflection area [m^2]
 % loss_factor     % loss % Loss of radar [dB]
@@ -17,18 +17,22 @@
 %          (4pi)^2 * d^4 * loss
 %
 	
-function power_recei = radar_equation(trans_gain,receiv_gain,RCS,loss_factor,distance)
+function power_recei = radar_equation(f_trans,c,RCS,loss_factor,distance)
 	
 	% Static value, anntenna
 	%power_trans = 5.6485e+03;
     
-    %13dB -> 18dB
-    power_trans = 6.30957e-2;
-    %18dB -> 13dB
-    trans_gain = 1.9952e-2;
+    %18dB
+    %power_trans = 6.30957e-2;
+    %13dB
+    %f_trans = 1.9952e-2;
     %16dB
-    receiv_gain = 3.9810e-2;
+    %receiv_gain = 3.9810e-2;
+	
+	lambda = c/f_trans;
 
-	power_recei = ((power_trans * trans_gain * receiv_gain * RCS) / ...
-				  (power(4*(pi), 2) * power(distance, 4) * loss_factor));
+	power_recei = ((lambda * RCS * nthroot((10^loss_factor), 10)) / (power(4*(pi), 2) * power(distance, 4)));
+				  
+	%power_recei = ((power_trans * f_trans * receiv_gain * RCS * 10*log10(loss_factor)) / ...
+				  %(power(4*(pi), 2) * power(distance, 4)));				  
 end
